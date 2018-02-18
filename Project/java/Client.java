006c63af02fd739c458871170ee8a08f145c900c
alexfullerton22 <alexfullerton22@gmail.com>
2018-02-17 17:47:34 -0800

    yaaasssss

--- Project/java/Client.java
+++ Project/java/Client.java
@@ -20,14 +20,14 @@ public class Client {
     server.setExecutor(null); // creates a default executor
     server.start();
 
-      Scanner scan = new Scanner(System.in);
-      System.out.print("Enter Player 1 name: ");
-      String nameone = scan.nextLine();
-      Player p1 = new Player(nameone);
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter Player 1 name: ");
+    String nameone = scan.nextLine();
+    Player p1 = new Player(nameone);
       
-      System.out.print("Enter Player 2 name: ");
-      String nametwo = scan.nextLine();
-      Player p2 = new Player(nametwo);
+    System.out.print("Enter Player 2 name: ");
+    String nametwo = scan.nextLine();
+    Player p2 = new Player(nametwo);
     
     Chess chess = new Chess();
   }
